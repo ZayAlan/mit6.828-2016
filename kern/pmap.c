e0949560c5ba4ceb682424cbc249a39fdd63b0f6
mrzhangboss <2529450174@qq.com>
2018-02-05 22:24:06 +0800

    Fix memory Permission Setting
    set PTE_U

--- kern/pmap.c
+++ kern/pmap.c
@@ -176,7 +176,7 @@ mem_init(void)
 	//      (ie. perm = PTE_U | PTE_P)
 	//    - pages itself -- kernel RW, user NONE
 	// Your code goes here:
-	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages),PTE_W);
+	boot_map_region(kern_pgdir, UPAGES, PTSIZE, PADDR(pages), PTE_U);
 
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
